6b79c085133d6c6b10a369d4e2f86cc3ea18b651
Pavlo Popeskul <peterjj91@gmail.com>
2022-06-03 14:20:45 +0300

    hw11/network: added tests

---
+++ hw11/pkg/index/cache/cache_test.go
@@ -0,0 +1,57 @@
+package cache
+
+import (
+	"go-search/hw11/pkg/crawler"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	m.Run()
+}
+
+func TestNew(t *testing.T) {
+	index := New()
+	if index == nil {
+		t.Errorf("New() returns nil")
+	}
+}
+
+func TestAdd(t *testing.T) {
+	index := New()
+	docs := []crawler.Document{
+		{ID: 1, Title: "title1"},
+		{ID: 2, Title: "title2"},
+	}
+	index.Add(docs)
+	if len(index.store) != 2 {
+		t.Errorf("Add() failed")
+	}
+}
+
+func TestSearch(t *testing.T) {
+	index := New()
+	docs := []crawler.Document{
+		{ID: 1, Title: "title1"},
+		{ID: 2, Title: "title2"},
+	}
+	index.Add(docs)
+	if len(index.Search("title1")) != 1 {
+		t.Errorf("Search() failed")
+	}
+}
+
+func Test_splitWords(t *testing.T) {
+	s := "title1"
+	words := splitWords(s)
+	if len(words) != 1 {
+		t.Errorf("splitWords() failed")
+	}
+}
+
+func Test_isExist(t *testing.T) {
+	ids := []int{1, 2, 3}
+	item := 1
+	if !isExist(ids, item) {
+		t.Errorf("isExist() failed")
+	}
+}
