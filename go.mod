9eadcbcc0e566875e17dff10e37bb74f48f6c251
Pavlo Popeskul <peterjj91@gmail.com>
2022-06-03 18:42:46 +0300

    hw12/webapp: basic

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module go-search
 go 1.18
 
 require golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4
+
+require github.com/gorilla/mux v1.8.0
---
+++ hw12/pkg/storage/memstore/memstore_test.go
@@ -0,0 +1,88 @@
+package memstore
+
+import (
+	"go-search/hw12/pkg/crawler"
+	"os"
+	"testing"
+)
+
+const fileName = "storage.json"
+
+var (
+	store       *DB
+	defaultDocs = []crawler.Document{
+		{ID: 1},
+		{ID: 2},
+	}
+)
+
+func TestMain(m *testing.M) {
+	store = New()
+	store.Add(defaultDocs)
+
+	m.Run()
+}
+
+func TestNew(t *testing.T) {
+	db := New()
+	if db == nil {
+		t.Errorf("New() = nil, want *DB")
+	}
+}
+
+func TestAdd(t *testing.T) {
+	store.Add([]crawler.Document{
+		{ID: 3},
+	})
+	if len(store.docs) != 3 {
+		t.Errorf("len(db.docs) = %d, want 3", len(store.docs))
+	}
+}
+
+func TestSearch(t *testing.T) {
+	tests := []struct {
+		ids   []int
+		count int
+	}{
+		{[]int{1}, 1},
+		{[]int{1, 2}, 2},
+		{[]int{100}, 0},
+	}
+
+	for _, tt := range tests {
+		docs := store.Search(tt.ids)
+		if len(docs) != tt.count {
+			t.Errorf("len(db.Search(%v)) = %d, want %d", tt.ids, len(docs), tt.count)
+		}
+	}
+}
+
+func TestRead(t *testing.T) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		t.Errorf("os.Open(%q) = %v, want nil", fileName, err)
+	}
+	defer f.Close()
+
+	docs, err2 := store.Read(f)
+	if err2 != nil {
+		t.Errorf("db.Read(nil) = %v, want nil", err2)
+	}
+
+	if len(docs) != 3 {
+		t.Errorf("len(db.docs) = %d, want 3", len(store.docs))
+	}
+}
+
+func TestWrite(t *testing.T) {
+	f, err := os.Create(fileName)
+	if err != nil {
+		t.Errorf("os.Create(%q) = %v, want nil", fileName, err)
+	}
+	defer f.Close()
+
+	_, err = store.Write(f, store.docs)
+	if err != nil {
+		t.Errorf("db.Write(nil) = %v, want nil", err)
+	}
+}
